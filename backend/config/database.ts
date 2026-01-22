42e2ce0da8ff19294fbfd623f2d749d0c1a87588
willianfonseca <wfonseca7@hotmail.com>
2026-01-22 12:26:23 +0100

    fix: render postgres ssl config

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -3,10 +3,9 @@ export default ({ env }) => ({
     client: 'postgres',
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      ssl: env.bool('DATABASE_SSL', true)
+        ? { rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false) }
+        : false,
     },
-    debug: false,
   },
 });
